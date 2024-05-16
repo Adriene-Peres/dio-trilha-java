c60f477752fefd1c42c5ef36bb6e603a2fb2f3ee
Adriene Peres <adriene_camargo@yahoo.com.br>
2024-05-16 10:59:10 -0300

    Sintaxe-java

---
+++ sintaxe-java/src/MinhaClasse.java
@@ -0,0 +1,12 @@
+public class MinhaClasse {
+    public static void main(String[] args) {
+        String nome = args[0];
+        String sobrenome = args[1];
+        String idade = args[2];
+        String altura = args[3];
+
+        System.out.println("Nome: " + nome + " " + sobrenome
+                        + "\nidade: " + idade + "\naltura: " + altura);
+
+    }
+}
